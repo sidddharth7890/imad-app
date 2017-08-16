5a19eee2215c8ed3c66f31487fd189bc35647aaa
sidddharth7890 <fultariyas@yahoo.com>
2017-08-16 20:06:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
+var img = document.getElementById('img');
+img.onclick = function () {
+img.style.marginLeft = '100px'    
+};
