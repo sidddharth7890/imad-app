4706772976c4859d89e23ee41fc3c1373b00a7ed
sidddharth7890 <fultariyas@yahoo.com>
2017-08-16 20:11:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,6 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
 var img = document.getElementById('madi');
 img.onclick = function () {
-img.style.marginLeft = '100px'    
-};
+    var interval = setInterval(moveLeft, 100); 
+    img.style.marginRight = '100px';    
+    };
