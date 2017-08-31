75fe9680f1597ed4bf72e768940b773287f5dcdc
sidddharth7890 <fultariyas@yahoo.com>
2017-08-31 08:53:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,25 +1,18 @@
-var submit = document.getElementById('submit_btn');
-submit.onclick = function() {
-    //Create a Request to counter Endpoint
-    var request = new XMLHttpRequest();
+//conuter code 
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function (){
     
-    //capture response and store it in variable
-    request.onreadystatechange = function() {
-      if(request.readyState === XMLHttpRequest.DONE){
-          if(request.status === 200 ){
-              alert('logged in successfully');
-          }else if (request.status === 403  ){
-              alert('incorrect username/password');
-          }else if (request.status === 500 ){
-              alert('something went wrong with server');
-          }
-        }
-    };
-
-    //Make the request
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    request.open('POST', 'http://rkvithlani.imad.hasura-app.io/login' ,true);
-    request.setRequestHeader('Content-Type','application/json');
-    request.send(JSON.stringify({username:username,password:password}));
-};
+    
+  //make request to the counter end point
+  
+  
+  //capture the response and store it in variable
+  
+  
+  //render the variable in the correct span
+    counter = counter + 1;
+    var span = document.getElementById('counter');
+    span.innerHTML = counter.toString();
+    
+}; 
