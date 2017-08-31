db0ae307f7459d4879cbff73b0d030aa2e02ae04
sidddharth7890 <fultariyas@yahoo.com>
2017-08-31 08:41:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var coumnter = 0;
+var counter = 0;
 app.get('/counter', function (req,res){
 counter = counter + 1;
 res.send(counter. toString());
